a0f953f45d3c53c533d44028c9b99bff84687248
Jaid <6216144+Jaid@users.noreply.github.com>
2024-04-04 05:12:51 +0200

    Improved output
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- x/build.ts
+++ x/build.ts
@@ -29,19 +29,25 @@ const makePkg = async () => {
   const outputPkg = {
     ...pkgSelection,
     type: `module`,
+    types: `more-types.d.ts`,
     exports: {
       ".": {
         types: `./more-types.d.ts`,
-        default: `./more-types.d.ts`,
+        default: `./blank.js`,
       },
     },
   }
   const outputFile = path.join(outputFolder, `package.json`)
   await fs.outputJson(outputFile, outputPkg)
 }
+const makeDummy = async () => {
+  const dummyFile = path.join(outputFolder, `blank.js`)
+  await fs.outputFile(dummyFile, `export {}`)
+}
 await Promise.all([
   makeDts(),
   makePkg(),
+  makeDummy(),
   fs.copy(`readme.md`, path.join(outputFolder, `readme.md`)),
   fs.copy(`license.txt`, path.join(outputFolder, `license.txt`)),
 ])
