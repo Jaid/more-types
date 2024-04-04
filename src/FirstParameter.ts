2192fa0bf41c831ffe446014be9df0d34f2a7510
Jaid <6216144+Jaid@users.noreply.github.com>
2024-04-04 05:42:06 +0200

    Added type `SecondParameter`
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/FirstParameter.ts
+++ src/FirstParameter.ts
@@ -1,1 +1,3 @@
 export type FirstParameter<FuctionGeneric> = FuctionGeneric extends (first: infer First, ...args: Array<any>) => any ? First : never
+
+export type SecondParameter<FuctionGeneric> = FuctionGeneric extends (first: any, second: infer Second, ...args: Array<any>) => any ? Second : never
