2dbb45bdcf470f9a760bda90a321451db9096c4c
Jaid <6216144+Jaid@users.noreply.github.com>
2024-04-04 05:43:24 +0200

    Added more Dicts
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/Dict.ts
+++ src/Dict.ts
@@ -1,1 +1,4 @@
 export type Dict<ValueGeneric = unknown> = Record<string, ValueGeneric>
+export type StringDict = Dict<string>
+export type NumberDict = Dict<number>
+export type BooleanDict = Dict<boolean>
